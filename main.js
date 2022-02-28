27129030f012a02313dfb7be7952cc7c23539a8e
kashyapa_niyarepola <kashyapaniyarepola@gmail.com>
2022-02-28 19:39:44 +0530

    welcome page

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+let win = null;
+
+const createWindow = () => {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        resizable: true,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile("index.html");
+};
+
+app.whenReady().then(createWindow);
